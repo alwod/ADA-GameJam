be50c8c2f4f1981c8144dcb57b89877078979b10
Mikha <mikharohrs@gmail.com>
2025-10-02 16:17:37 +0100

    Added basic enemy

---
+++ source/enemy/enemy.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var player = get_tree().get_first_node_in_group("Player")
+@export var movement_speed = 50.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	if not player:
+		return
+		
+	var direction = global_position.direction_to(player.global_position)
+	print("Direction to player:", direction)
+	velocity = direction * movement_speed
+	move_and_slide()
