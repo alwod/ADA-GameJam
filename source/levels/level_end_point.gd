ad782e2173effd970a40a09dedb86e410c0602ae
Mikha <mikharohrs@gmail.com>
2025-10-03 10:35:20 +0100

    start screen now displays score

--- source/levels/level_end_point.gd
+++ source/levels/level_end_point.gd
@@ -1,19 +1,16 @@
-extends Node
+extends Area2D
 
-var coins = 0;
-var game_over = false
-var game_win = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	game_over = false
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-func reset():
-	coins = 0
-	game_over = false
-	game_win = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	GameManager.game_win = true
+	get_tree().change_scene_to_file("res://start_screen.tscn")
--- source/start_screen.gd
+++ source/start_screen.gd
@@ -8,11 +8,11 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	check_if_game_over()
+	check_game_status()
 
 
 func _on_start_button_pressed() -> void:
-	GameManager.game_over = false
+	GameManager.reset()
 	get_tree().change_scene_to_file("res://levels/level_1.tscn")
 
 
@@ -26,10 +26,12 @@ func _on_exit_button_pressed() -> void:
 
 
 func _on_timer_timeout() -> void:
-	check_if_game_over()
+	check_game_status()
 	
-func check_if_game_over():
-	if !GameManager.game_over:
-		message_label.text = "Aqua Assets"
-	else:
+func check_game_status():
+	if GameManager.game_over:
 		message_label.text = "Game Over"
+	elif GameManager.game_win:
+		message_label.text = "You won! Score: " + str(GameManager.coins)
+	else:
+		message_label.text = "Aqua Assets"
